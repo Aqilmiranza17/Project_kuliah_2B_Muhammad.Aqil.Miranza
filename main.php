57345675cedf92f12edced2f233dbdf75f343b87
Aqilmiranza17 <aqilmiranza17.com>
2023-12-10 03:19:23 +0700

    changes login page themes

--- main.php
+++ main.php
@@ -18,7 +18,7 @@ $hasil = mysqli_fetch_array($query);
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
    <link rel="stylesheet" href="assets/css/style.css">
-   <title>Document</title>
+   <title>InventoryApotek</title>
 
    <!-- js untuk active pada sidebar -->
    <script>
@@ -58,11 +58,7 @@ $hasil = mysqli_fetch_array($query);
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
       crossorigin="anonymous"></script>
-
-   <!-- feather icon -->
-   <script>
-      feather.replace();
-   </script>
+   <!-- js end from bootstrap -->
 </body>
 
 </html>
