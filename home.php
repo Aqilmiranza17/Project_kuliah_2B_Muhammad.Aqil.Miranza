f2df33f84c15cdc06faee9adeaf0a138f450719d
Aqilmiranza17 <aqilmiranza17.com>
2023-12-11 13:52:18 +0700

    Make Time In homepage

--- home.php
+++ home.php
@@ -1,11 +1,23 @@
+<?php
+// Set the time zone to Jakarta
+date_default_timezone_set('Asia/Jakarta');
+
+// Get the current date and time in Jakarta's time zone
+$currenttimes = date("H:i:s");
+$currentyears = date("Y-m-d");
+
+?>
+
+
 <div class="col-lg-10 d-flex align-items-start justify-content-center mt-3">
    <div class="card w-75 mb-3 border-0">
       <div class="card-body">
-         <h5 class="card-title">Dashboard</h5>
-         <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae nihil possimus
-            quaerat reiciendis ea esse sint dolore expedita inventore sit velit autem, ab porro dolorem ipsum.
-            Praesentium necessitatibus sint totam?</p>
-         <a href="#" class="btn btn-primary">Button</a>
+         <h1 class="text-center">
+            <?= $currentyears ?>
+         </h1>
+         <h1 class="text-center">
+            <?= $currenttimes ?>
+         </h1>
       </div>
    </div>
 </div>
